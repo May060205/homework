f434c827d6c53a969b37f9dd9b898ab9b1d85874
May060205 <2064400848@qq.com>
2026-06-06 16:23:26 +0800

    修改代码问题

--- ex01/HelloWorld.c
+++ ex01/HelloWorld.c
@@ -3,8 +3,8 @@ int main()
 {
     printf("Hello World!\n");
     char str[100];
-    printf("please input：");
+    printf("please input:");
     scanf("%s", str);  
-    printf("your input：%s\n", str);
+    printf("your input:%s\n", str);
     return 0;
 }
